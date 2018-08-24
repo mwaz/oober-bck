9aa19e51d5f8e1ac52977d645060c3f0ab6ad959
mwaz <amby.mwaz@gmail.com>
2018-08-24 08:35:05 +0300

    [fx]signup validation

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -2,10 +2,31 @@ const moongoose = require("mongoose");
 
 const UserSchema = moongoose.Schema(
   {
-    username: String,
-    email: String,
-    password: String,
-    confirmPassword: String
+    username: {
+      type: String,
+      min: [4, "Username too short"],
+      max: 12,
+      required: [true, "Kindly provide a username"]
+    },
+    password: {
+      type: String,
+      min: [6, "Password too short"],
+      max: 50,
+      required: [true, "Kindly provide a valid password"]
+    },
+    email: {
+      type: String,
+      min: [3, "Email too short"],
+      max: 50,
+      required: [false, "Kindly provide a valid email"],
+      unique: true
+    },
+    confirmPassword: {
+      type: String,
+      min: [6, "Password too short"],
+      max: 50,
+      required: [true, "Kindly provide a valid confirm password"]
+    }
   },
   {
     timestamps: true
